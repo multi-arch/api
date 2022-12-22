32164c3cb8fd17e8ceaa6c873215820425074bef
Jeremy Poulin <jaypoulz@gmail.com>
2022-12-22 17:12:07 -0500

    Began writing validator for nodeAffinity

--- pkg/validation/internal/multiarch.go
+++ pkg/validation/internal/multiarch.go
@@ -26,11 +26,13 @@ import (
 //
 // ### What is checked?
 //
-// On this check, we aggregate the archetype(s) and OS(s) provided via the labels and those which are found by checking the images so that, we can check:
+// On this check, we aggregate the platform architecture(s) and OS(s) provided via the labels and those which are found by checking the images so that, we can check:
 //
 // - If your CSV is missing labels
 //
-// - If your Operator bundle specifies images which do not support all archetypes found for your Operator image(s) (probably supported by your project)
+// - If your Operator bundle specifies images which do not support all architectures found for your Operator image(s) (probably supported by your project)
+//
+// - If your deployment spec follows the best practice of setting nodeAffinity to ensure image(s) are only scheduled on compatible platform nodes.
 //
 // Note: To better guess the case scenarios where authors might have missed the labels, the following check will verify all architectures supported by the Operator image(s). However, by looking at the CSV we are not able to ensure what is the Operator image because this info is not provided. Therefore, we know by SDK the Operator image container will be called manager.
 //
@@ -72,6 +74,8 @@ type multiArchValidator struct {
 	managerArchs map[string]string
 	// managerOs contains a map of the OSes found
 	managerOs map[string]string
+	// imageNodeAffinity maps the image to its nodeAffinity boundaries
+	imageNodeAffinity map[string][]
 	// Store the bundle load
 	bundle *manifests.Bundle
 	// containerTool defines the container tool which will be used to inspect the allOtherImages
@@ -96,6 +100,13 @@ type manifestData struct {
 type platform struct {
 	Architecture string `json:"architecture"`
 	OS           string `json:"os"`
+}
+
+// nodeSelectorTerm allows inspection of nodeAffinity boundaries
+type nodeSelectorTerm struct {
+	Key string `json:"key"`
+	Operator string `json:"operator"`
+	Values string[] `json:"values"`
 }
 
 func multipleArchitecturesValidate(objs ...interface{}) (results []errors.ManifestResult) {
@@ -184,7 +195,7 @@ func (data *multiArchValidator) validate() {
 	data.managerImages = data.inspectImages(data.managerImages)
 	data.allOtherImages = data.inspectImages(data.allOtherImages)
 	data.loadAllPossibleArchSupported()
-	data.loadAllPossibleSoSupported()
+	data.loadAllPossibleOsSupported()
 	data.doChecks()
 }
 
@@ -214,26 +225,42 @@ func (data *multiArchValidator) loadImagesFromCSV() {
 	// authors that when forgotten to use add the labels
 	// because we found images that provides more support
 	data.managerImages = make(map[string][]platform)
+
+	// We will store the nodeAffinity information in the CSV as we encounter it
+        data.imageNodeAffinty = make(map[string][]platform)
+
 	for _, v := range data.bundle.CSV.Spec.InstallStrategy.StrategySpec.DeploymentSpecs {
 		foundManager := false
 		// For the default scaffold we have a container called manager
 		for _, c := range v.Spec.Template.Spec.Containers {
-			if c.Name == "manager" && len(data.managerImages[c.Image]) == 0 {
+			// Skip duplicate images
+			if len(data.managerImages[c.Image]) == 0 {
+				continue
+			}
+
+			// Store the manager container information for later validation
+			if c.Name == "manager" {
 				data.managerImages[c.Image] = append(data.managerImages[c.Image], platform{})
 				data.managerImagesString = append(data.managerImagesString, c.Image)
-				foundManager = true
-				break
+			        foundManager = true
 			}
+
+			// Collect nodeAffinity boundaries for all images
+			data.imageNodeAffinity[c.Image] = append(extractNodeAffinityPlatforms(c), platform{})
 		}
+
 		// If we do not find a container called manager then we
 		// will add all from the Deployment Specs which is not the
 		// kube-rbac-proxy image scaffold by default
 		if !foundManager {
 			for _, c := range v.Spec.Template.Spec.Containers {
-				if c.Name != default_container_scaffold_by_sdk && len(data.managerImages[c.Image]) == 0 {
-					data.managerImages[c.Image] = append(data.managerImages[c.Image], platform{})
-					data.managerImagesString = append(data.managerImagesString, c.Image)
+				// Skip kube-rbac-proxy or already added images
+				if c.Name == default_container_scaffold_by_sdk || len(data.managerImages[c.Image]) == 0 {
+					continue
 				}
+
+				data.managerImages[c.Image] = append(data.managerImages[c.Image], platform{})
+				data.managerImagesString = append(data.managerImagesString, c.Image)
 			}
 		}
 	}
@@ -242,10 +269,12 @@ func (data *multiArchValidator) loadImagesFromCSV() {
 	if data.bundle.CSV.Spec.InstallStrategy.StrategySpec.DeploymentSpecs != nil {
 		for _, v := range data.bundle.CSV.Spec.InstallStrategy.StrategySpec.DeploymentSpecs {
 			for _, c := range v.Spec.Template.Spec.Containers {
-				// If not be from manager the add
-				if len(data.managerImages[c.Image]) == 0 {
-					data.allOtherImages[c.Image] = append(data.allOtherImages[c.Image], platform{})
+				// Skip images in the manager image list
+				if len(data.managerImages[c.Image]) != 0 {
+					continue
 				}
+
+				data.allOtherImages[c.Image] = append(data.allOtherImages[c.Image], platform{})
 			}
 		}
 	}
@@ -255,8 +284,55 @@ func (data *multiArchValidator) loadImagesFromCSV() {
 	}
 }
 
+// extractNodeAffinityPlatforms scans the deployment spec for
+// affinity.nodeAffinity.requiredDuringSchedulingIngoredDringExecution.nodeSelectorTerms
+// that set platform requirements for kubernetes.io/arch and kubernetes.io/os labels
+func extractNodeAffinityPlatforms(spec) ([]platforms) {
+	if !spec.Affinity || !spec.Affinity.NodeAffinity || !spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution || !spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms {
+		// No platforms set
+		return []platform{}
+	}
+
+	var terms = spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms
+	for _, t := range terms {
+		var arches = make([]string)
+		var os = make([]string)
+		var archSet = false
+		var osSet = false
+		var otherTerm = false
+
+		for _, e : range t.MatchesExpressions {
+			if e.Key != "In": {
+				otherTerm = true
+				break
+			}
+
+			if e.Key == "kubernetes.io/arch" {
+				archSet = true
+				arches = e.Values
+				continue
+			} else if e.Key == "kubernetes.io/os" {
+				osSet = true
+				os = e.Values
+				continue
+			}
+
+			otherTerm = true
+			break
+		}
+	}
+
+	for _, o := range os {
+		for _, a := range arches {
+			append(platforms, platform{architecture: a, os: o})
+		}
+	}
+
+	return platforms
+}
+
 // runManifestInspect executes the command for we are able to check what
-// are the Architecture(s) and SO(s) supported per each image found
+// are the Architecture(s) and OS(s) supported per each image found
 func runManifestInspect(image, tool string) (manifestInspect, error) {
 	cmd := exec.Command(tool, "pull", image)
 	_, err := runCommand(cmd)
@@ -297,7 +373,7 @@ func (data *multiArchValidator) inspectImages(images map[string][]platform) map[
 			if err != nil {
 				data.warns = append(data.warns, fmt.Errorf("unable to inspect the image (%s) : %s", k, err))
 
-				// We set the Arch and SO as error for we are able to deal witth these cases further
+				// We set the Arch and OS as error for we are able to deal witth these cases further
 				// Se that make no sense we raise a warning to notify the user that the image
 				// does not provide some kind of support only because we were unable to inspect it.
 				// Be aware that the validator raise warnings for all cases scenarios to let
@@ -325,14 +401,17 @@ func (data *multiArchValidator) doChecks() {
 	// on the check above. The following check raise a warning when it is possible to check that the Operator
 	// manager image(s) supports architecture(s) not defined via labels. Therefore, it shows like the labels are missing
 	data.checkMissingLabelsForArchs()
-	data.checkMissingLabelsForSO()
+	data.checkMissingLabelsForOS()
 	// the following check will raise warnings when is possible to verify that the images defined in the CSV
 	// does not provide the same architecture(s) supported by the Operator manager or defined via the labels
 	data.checkMissingSupportForOtherImages()
+	// the following check will raise warnings when nodeAffinity isn't set to ensure that the pod spec will only
+	// target nodes matching the platforms (os/arch) specified in the manifest.
+	data.checkMissingNodeAffinityBoundaries()
 }
 
 // checkMissingSupportForOtherImages checks if any image is missing some arch or so found
-// (probably the image should support the arch or SO )
+// (probably the image should support the arch or OS )
 func (data *multiArchValidator) checkMissingSupportForOtherImages() {
 	for image, plaformFromImage := range data.allOtherImages {
 		listArchNotFound := []string{}
@@ -367,7 +446,7 @@ func (data *multiArchValidator) checkMissingSupportForOtherImages() {
 					data.managerImagesString))
 		}
 
-		listAllSoNotFound := []string{}
+		listAllOsNotFound := []string{}
 		for archOSList := range data.managerOs {
 			found := false
 			for _, imageData := range plaformFromImage {
@@ -384,27 +463,55 @@ func (data *multiArchValidator) checkMissingSupportForOtherImages() {
 				}
 			}
 			if !found && archOSList != "error" {
-				listAllSoNotFound = append(listAllSoNotFound, archOSList)
+				listAllOsNotFound = append(listAllOsNotFound, archOSList)
 			}
 		}
-		if len(listAllSoNotFound) > 0 {
-			sort.Strings(listAllSoNotFound)
+		if len(listAllOsNotFound) > 0 {
+			sort.Strings(listAllOsNotFound)
 			data.warns = append(data.warns,
 				fmt.Errorf("check if the image %s should not support %q. "+
-					"Note that this CSV has labels for this SO(s) "+
+					"Note that this CSV has labels for this OS(s) "+
 					"Your manager image %q are providing this support OR the CSV is configured via labels "+
 					"to support it. Then, please verify if this image should not support it",
 					image,
-					listAllSoNotFound,
+					listAllOsNotFound,
 					data.managerImagesString))
 		}
 	}
 }
 
-// verify if 1 or more allOtherImages has support for a SO not defined via the labels
-// (probably the label for this SO is missing )
-func (data *multiArchValidator) checkMissingLabelsForSO() {
-	notFoundSoLabel := []string{}
+// checkMissingNodeAffinityBoundaries checks if any image is missing nodeAffinity block corresponding to
+// the supports os/arch platforms in the manifest.
+func (data *multiArchValidator) checkMissingNodeAffinityBoundaries() {
+	for image, plaformFromImage := range data.allOtherImages {
+		listNodeAffinityNotFound := []string{}
+		for archFromList := range data.managerArchs {
+			found := false
+			for _, imageData := range plaformFromImage {
+				// Ignore the case when the Plataform.Architecture == "error" since that means
+				// that was not possible to inspect the image
+				if imageData.Architecture == "error" {
+					found = true
+					break
+				}
+
+				if imageData.Architecture == archFromList {
+					found = true
+					break
+				}
+			}
+			if !found && archFromList != "error" {
+				listArchNotFound = append(listArchNotFound, archFromList)
+			}
+		}
+
+	}
+}
+
+// verify if 1 or more allOtherImages has support for an OS not defined via the labels
+// (probably the label for this OS is missing )
+func (data *multiArchValidator) checkMissingLabelsForOS() {
+	notFoundOsLabel := []string{}
 	for supported := range data.managerOs {
 		found := false
 		for _, infra := range data.infraCSVOSLabels {
@@ -417,24 +524,24 @@ func (data *multiArchValidator) checkMissingLabelsForSO() {
 		if !found && supported != "error" {
 			// if the only arch supported is linux then,  we should not ask for the label
 			if !(supported == "linux" && len(data.managerOs) == 1 && len(data.managerOs["linux"]) > 0) {
-				notFoundSoLabel = append(notFoundSoLabel, supported)
+				notFoundOsLabel = append(notFoundOsLabel, supported)
 			}
 
 		}
 	}
 
-	if len(notFoundSoLabel) > 0 {
+	if len(notFoundOsLabel) > 0 {
 		// We need to sort, otherwise it is possible verify in the tests that we have
 		// this message as result
-		sort.Strings(notFoundSoLabel)
+		sort.Strings(notFoundOsLabel)
 		data.warns = append(data.warns,
-			fmt.Errorf("check if the CSV is missing the label (%s<value>) for the SO(s): %q. "+
+			fmt.Errorf("check if the CSV is missing the label (%s<value>) for the OS(s): %q. "+
 				"Be aware that your Operator manager image %q provides this support. "+
-				"Thus, it is very likely that you want to provide it and if you support more than linux SO you MUST,"+
+				"Thus, it is very likely that you want to provide it and if you support more than linux OS you MUST,"+
 				"use the required labels for all which are supported."+
 				"Otherwise, your solution cannot be listed on the cluster for these architectures",
 				operatorFrameworkOSLabel,
-				notFoundSoLabel,
+				notFoundOsLabel,
 				data.managerImagesString))
 	}
 }
@@ -499,16 +606,16 @@ func (data *multiArchValidator) loadAllPossibleArchSupported() {
 	}
 }
 
-// loadAllPossibleSoSupported will verify all SO that this bundle can support
-// for then, we aare able to check if it is missing labels.
+// loadAllPossibleOsSupported will verify all OS that this bundle can support
+// for then, we are able to check if it is missing labels.
 // Note:
-// - we check what are the SO of all allOtherImages informed
-// - we ensure that the linux SO will be added when none so labels were informed
-// - we check all labels to know what are the SO(s) to obtain the list of them which the bundle is defining
-func (data *multiArchValidator) loadAllPossibleSoSupported() {
+// - we check what are the OS of all allOtherImages informed
+// - we ensure that the linux OS will be added when none so labels were informed
+// - we check all labels to know what are the OS(s) to obtain the list of them which the bundle is defining
+func (data *multiArchValidator) loadAllPossibleOsSupported() {
 	// Add the values provided via label
 	for _, v := range data.infraCSVOSLabels {
-		label := extractValueFromSoLabel(v)
+		label := extractValueFromOsLabel(v)
 		data.managerOs[label] = label
 	}
 
@@ -517,7 +624,7 @@ func (data *multiArchValidator) loadAllPossibleSoSupported() {
 		data.managerOs["linux"] = "linux"
 	}
 
-	// Get all SO from the provided allOtherImages
+	// Get all OS from the provided allOtherImages
 	for _, imageData := range data.managerImages {
 		for _, plataform := range imageData {
 			if len(plataform.OS) > 0 {
@@ -527,7 +634,7 @@ func (data *multiArchValidator) loadAllPossibleSoSupported() {
 	}
 }
 
-// checkSupportDefined checks if all allOtherImages supports the ARCHs and SOs defined
+// checkSupportDefined checks if all allOtherImages supports the ARCHs and OSs defined
 func (data *multiArchValidator) checkSupportDefined() {
 	configuredS0 := []string{}
 	if len(data.infraCSVOSLabels) == 0 {
@@ -535,7 +642,7 @@ func (data *multiArchValidator) checkSupportDefined() {
 	}
 
 	for _, label := range data.infraCSVOSLabels {
-		configuredS0 = append(configuredS0, extractValueFromSoLabel(label))
+		configuredS0 = append(configuredS0, extractValueFromOsLabel(label))
 	}
 
 	configuredArch := []string{}
@@ -604,14 +711,14 @@ func (data *multiArchValidator) checkSupportDefined() {
 			sort.Strings(images)
 			data.errors = append(data.errors,
 				fmt.Errorf("not all images specified are providing the support described via the CSV labels. "+
-					"Note that (SO.architecture): (%s) was not found for the image(s) %s",
+					"Note that (OS.architecture): (%s) was not found for the image(s) %s",
 					platform, images))
 		}
 	}
 }
 
-// extractValueFromSoLabel returns only the value of the SO label (i.e. linux)
-func extractValueFromSoLabel(v string) string {
+// extractValueFromOsLabel returns only the value of the OS label (i.e. linux)
+func extractValueFromOsLabel(v string) string {
 	label := strings.ReplaceAll(v, operatorFrameworkOSLabel, "")
 	return label
 }
